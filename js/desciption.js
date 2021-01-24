77500477718da57cd265d07f718ecd0a3c76b09d
JamilaOthman <mema20330@gmail.com>
2021-01-24 14:30:34 +0200

    3

--- js/desciption.js
+++ js/desciption.js
@@ -1,2 +1,15 @@
 'use strict';
+import { superhero } from './superheroData.js';
 const content = document.querySelector('.content');
+window.addEventListener('DOMContentLoaded', () => {
+    const choosenhero = superhero.find((hero) => {
+        return hero.id === Number(localStorage.getItem('value'));
+    });
+    content.innerHTML = `<div class="img-container">
+                    <img src=${choosenhero.src} alt=${choosenhero.name}>
+                </div>
+                <div class="info-container">
+                    <h2>${choosenhero.name}</h2>
+                    <p>${choosenhero.bio} ${choosenhero.power}</p>
+                </div>`;
+});
