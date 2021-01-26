c3278a8140c9e0cd821eb4678aa2c5de82724e59
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-01-27 00:47:54 +0200

    edit the description page

--- js/superheroData.js
+++ js/superheroData.js
@@ -3,6 +3,8 @@ export let superhero = [
 		id: 1,
 		name: 'Batman',
 		src: 'img/batman.jpg',
+		pics: ['batman/1.jpg', 'batman/2.jpg', 'batman/3.jpg', 'batman/4.jpg'],
+		color: ['rgb(36, 36, 36)', 'rgb(255, 190, 59)', 'rgb(255, 190, 59)'],
 		bio:
 			'It’s difficult to think of any superhero who is loved more than Bruce Wayne’s alter-ego. He remains one of the most popular superheroes around over 70 years since his first incarnation in 1939, arriving not long after the creation of the first Marvel superhero, the Sub-Mariner.  The character has come a long way from the ‘Boom!’ ‘Pow’ slapstick style of the 60’s and a new trilogy of Batman films are currently in production with Twilight star Robert Pattinson starring as the man in the winged latex suit.',
 		gender: 'male',
@@ -15,6 +17,13 @@ export let superhero = [
 		id: 2,
 		name: 'Wonder-Woman',
 		src: 'img/wonder-woman.jpg',
+		pics: [
+			'wonder-woman/1.jpg',
+			'wonder-woman/2.jpg',
+			'wonder-woman/3.jpg',
+			'wonder-woman/4.jpg',
+		],
+		color: ['rgb(236, 30, 51)', 'rgb(251, 205, 171)', 'rgb(251, 205, 171)'],
 		bio:
 			'Top has to be the one and only Wonder Woman.  Those born in the early 70’s will remember the iconic imprint left on them thanks to Lynda Carter as Wonder Woman.  She proved the world would be a better place if run by women.  She was created in the early 1940s at a time when comics were dominated by superpower-endowed male characters. Wonder Woman was invented by William Moulton Marston, an American Psychologist/ Inventor / Writer.  He invented the first prototype of the lie detector interestingly. It is thought it was his wife who gave him the idea of a woman superhero. William pitched the idea to Max Gaines, co-founder with Jack Liebowitz of All-American Publications and Wonder Woman was born.  All American Publications went on to become DC Comics.',
 		gender: 'female',
@@ -28,6 +37,13 @@ export let superhero = [
 		id: 3,
 		name: 'Superman',
 		src: 'img/super-man.jpg',
+		pics: [
+			'super-man/1.jpg',
+			'super-man/2.jpg',
+			'super-man/3.jpg',
+			'super-man/4.jpg',
+		],
+		color: ['rgb(9, 57, 121)', 'red', 'red'],
 		bio:
 			'One of the most popular superheros today is still Superman.  If anyone can challenge the popularity of Batman then it would have to be Superman. He has been around one year longer and has proven to be just as enduring. We even got to see the two clash in the Batman vs Superman film made in 2016. Christopher Reeves 1970s incarnation remains the version of Superman most people still refer to today. Superman is also probably the strongest superhero around (although Captain Marvel DC may have something to say about that).',
 		gender: 'male',
@@ -40,6 +56,8 @@ export let superhero = [
 		id: 4,
 		name: 'Wasp',
 		src: 'img/wasp.jpg',
+		pics: ['wasp/1.jpg', 'wasp/2.jpg', 'wasp/3.jpg', 'wasp/4.jpg'],
+		color: ['rgb(30, 10, 3)', 'rgb(193, 161, 78)', 'rgb(193, 161, 78)'],
 		bio:
 			'anet Van Dyne was not only the first female Avenger, and a founder, but also the hero who named the team when they first formed. Though she started out as something of a sidekick to her on-again-off-again (currently off-again) paramour Hank Pym, Janet quickly became a hero in her own right, leading the Avengers several times, and often acting as the team"s moral center.',
 		gender: 'female',
@@ -53,6 +71,13 @@ export let superhero = [
 		id: 5,
 		name: 'Spider-Man',
 		src: 'img/spiderman.jpg',
+		pics: [
+			'spider-man/1.jpg',
+			'spider-man/2.jpg',
+			'spider-man/3.jpg',
+			'spider-man/4.jpg',
+		],
+		color: ['rgb(39, 46, 98)', 'rgb(254, 0, 0)', 'rgb(254, 0, 0)'],
 		bio:
 			'As far as Marvel superheroes go, there is no one more popular than Spider-Man. He first appeared in 1969 and easily made the jump from comic book hero to TV star and is now taking centre stage in the Marvel Cinematic Universe. Since 2002 alone there have been 7 Spider-Man films (played by three different actors) with another planned to arrive in July of 2021.',
 		gender: 'male',
@@ -65,6 +90,8 @@ export let superhero = [
 		id: 6,
 		name: 'Storm',
 		src: 'img/storm-woman.jpg',
+		pics: ['storm/1.jpg', 'storm/2.jpg', 'storm/3.jpg', 'storm/4.jpg'],
+		color: ['rgb(169, 178, 237)', 'rgb(240, 189, 96)', 'rgb(240, 189, 96)'],
 		bio:
 			'Storm is a fictional superhero who again first appeared appearing in the American comic books published by Marvel Comics.  The character Storm was created by writer Len Wein and artist Dave Cockrum and she first appeared in  Giant-Size X-Men in May 1975.  She is one of the more prominent characters in the X-Men series.  The first actress to portray Storm was Alexandra Shipp in the 2016 film X-Men: Apocalypse.  In the 2000 film Halle Berry played the character.  She also went on to play that role in films X2, X-Men: The Last Stand, and X-Men: Days of Future Past.',
 		gender: 'female',
@@ -79,6 +106,13 @@ export let superhero = [
 		id: 7,
 		name: 'Iron-Man',
 		src: 'img/iron-man.jpg',
+		pics: [
+			'iron-man/1.jpg',
+			'iron-man/2.jpg',
+			'iron-man/3.jpg',
+			'iron-man/4.jpg',
+		],
+		color: ['rgb(220, 20, 5)', 'rgb(252, 254, 5)', 'rgb(252, 254, 5)'],
 		bio:
 			'While he wasn’t the favourite superhero in comic book terms, thanks to Robert Downey Jr.’s performances he has become one of the standout Avengers superheroes. It’s hard not to be won over by the handsome and over-confident Tony Stark, who always manages to live up to his ego – not matter how large it may become',
 		gender: 'male',
@@ -92,6 +126,8 @@ export let superhero = [
 		id: 8,
 		name: 'Wolverine',
 		src: 'img/wolverine.jpg',
+		pics: ['wolv/1.jpg', 'wolv/2.jpg', 'wolv/3.jpg', 'wolv/4.jpg'],
+		color: ['rgb(25, 117, 161)', 'rgb(255, 208, 96)', 'rgb(255, 208, 96)'],
 		bio:
 			'While Wolverine isn’t as old as some of those listed above, he quickly gained a reputation as part of the X-Men. His grumpy, no nonsense demeanour was captured perfectly by Hugh Jackman who played the character in countless X-Men and solo films for almost 20 years. Those sharp claws and wolf-like senses are loved by millions of fans across the world and we shouldn’t be surprised if someone else takes over the mantle within the next few years.',
 		gender: 'male',
@@ -105,6 +141,8 @@ export let superhero = [
 		id: 9,
 		name: 'Jean-Grey',
 		src: 'img/jean.jpg',
+		pics: ['jean/1.jpg', 'jean/2.jpg', 'jean/3.jpg', 'jean/4.jpg'],
+		color: ['rgb(18, 26, 38)', 'rgb(255, 208, 96)', 'rgb(255, 208, 96)'],
 		bio:
 			'The first Marvel Girl or now X-Woman Jean Grey pretty much represents the whole X-Men franchise.  She has been known under the aliases Marvel Girl, Phoenix, and Dark Phoenix.  Jean Grey is a mutant with powerful telekinetic and telepathic abilities. She studied at Xavier’s School for Gifted Youngsters and trained as a second-generation member of the X-Men under the tutelage of Professor X. She first appeared in The X-Men Comic Books in March 1964. Most recently the popular actress Sophie Turner (star from Game of Thrones) portrayed Jean Grey in the film X-Men: Apocalypse and X-Men: Dark Phoenix in 2019.  You can never keep Jean down as like a phoenix she always rises from the ashes.',
 		gender: 'female',
@@ -117,6 +155,13 @@ export let superhero = [
 		id: 10,
 		name: 'Captain-America',
 		src: 'img/captin-america.jpg',
+		pics: [
+			'captain-america/1.jpg',
+			'captain-america/2.jpg',
+			'captain-america/3.jpg',
+			'captain-america/4.jpg',
+		],
+		color: ['rgb(37, 102, 151)', 'rgb(124, 21, 21)', 'rgb(124, 21, 21)'],
 		bio:
 			'As America answered the call to enter the Second World War, Marvel created what would go on to become one of their most iconic characters. Captain America was a supersoldier who fought against the Nazis and their supporters during the wartime period. During the ‘40s and ‘50s he grew to become an all-American hero for kids to look up to as the country’s’ economic wealth continued to grow, and decades later Chris Evans was perfectly cast as the square-jawed, blue-eyed hunk in the Marvel Universe.',
 		gender: 'male',
@@ -131,6 +176,13 @@ export let superhero = [
 		id: 11,
 		name: 'Scarlet-Witch',
 		src: 'img/scarlet-witch.jpg',
+		pics: [
+			'scarlet-witch/1.jpg',
+			'scarlet-witch/2.jpg',
+			'scarlet-witch/3.jpg',
+			'scarlet-witch/4.jpg',
+		],
+		color: ['rgb(211, 24, 25)', 'rgb(31, 22, 25)', 'rgb(31, 22, 25)'],
 		bio:
 			'Scarlet Witch is another fictional female superhero who first appeared in the American Marvel Comic books.  This diva woman superhero is one of the most dangerous heroes in the Marvel world.  The character was created by writer Stan Lee and artist Jack Kirby.  She first appeared in The X-Men Comic Books in March 1964. The character has appeared in a number of animated films and  television series. Elizabeth Olsen will reprise the role in the upcoming Disney series WandaVision (2020) and the film Doctor Strange in the Multiverse of Madness (2021).',
 		gender: 'female',
@@ -144,6 +196,8 @@ export let superhero = [
 		id: 12,
 		name: 'The-Hulk',
 		src: 'img/hulk.jpg',
+		pics: ['hulk/1.jpg', 'hulk/2.jpg', 'hulk/3.jpg', 'hulk/4.jpg'],
+		color: ['rgb(80, 24, 65)', 'rgb(0, 171, 20)', 'rgb(29, 81, 36)'],
 		bio:
 			'Everyone has a green monster lurking inside that comes out when they get angry and The Hulk represents that perfectly. Although, we’re probably not as insanely strong or destructive as this green giant.  He first came to life in comic books in 1962 and has been played by countless actors over the years on both TV and film. The Hulk and The Incredible Hulk were released in 2003 and 2008 respectively, although are not remembered too fondly by fans.',
 		gender: 'male',
@@ -157,6 +211,13 @@ export let superhero = [
 		id: 13,
 		name: 'Black-Widow',
 		src: 'img/black-widow.jpg',
+		pics: [
+			'black-widow/1.jpg',
+			'black-widow/2.jpg',
+			'black-widow/3.jpg',
+			'black-widow/4.jpg',
+		],
+		color: ['black', 'rgb(249, 246, 229)', 'rgb(249, 246, 229)'],
 		bio:
 			'Black Widow has been around as a character since the 60s, but it is only recently that she is become a particularly prominent heroine in the Marvel Universe, thanks in large part to her role as a founding member of the cinematic Avengers.',
 		gender: 'female',
@@ -171,6 +232,8 @@ export let superhero = [
 		id: 14,
 		name: 'Thor',
 		src: 'img/thor.jpg',
+		pics: ['thor/1.jpg', 'thor/2.jpg', 'thor/3.jpg', 'thor/4.jpg'],
+		color: ['rgb(135, 0, 0)', 'rgb(195, 193, 193)', 'rgb(195, 193, 193)'],
 		bio:
 			'The God of Thunder arrived at around the same time as the Hulk in 1962. Norse mythology is used as the basis of a character who can wield lightening and smash almost anything with his trusty hammer (which also allows him to fly). Marvel have perfectly cast the right actors for all of the roles in their superhero universe and it’s hard to imagine another actor other than Chris Hemsworth playing the character.',
 		gender: 'male',
@@ -185,6 +248,13 @@ export let superhero = [
 		id: 15,
 		name: 'Captain-Marvel',
 		src: 'img/captin-marvil.jpg',
+		pics: [
+			'captain-marvel/1.jpg',
+			'captain-marvel/2.jpg',
+			'captain-marvel/3.jpg',
+			'captain-marvel/4.jpg',
+		],
+		color: ['rgb(41, 59, 83)', 'rgb(196, 155, 93)', 'rgb(196, 155, 93)'],
 		bio:
 			'Carol Danvers is just about the most powerful woman in the Marvel Universe, and is arguably the publisher is top female hero. With cosmic powers, a background as a fighter pilot, a high profile movie, and that crucial Avengers membership, she is everything great about superheroes wrapped up in one sleek package.It"s no wonder the next phase of the Marvel Cinematic Universe will likely put Carol front and center, as one of the pillars of the most popular superhero brand in the world.',
 		gender: 'female',
@@ -198,6 +268,13 @@ export let superhero = [
 		id: 16,
 		name: 'The-Flash',
 		src: 'img/the-flash.jpg',
+		pics: [
+			'the-flash/1.jpg',
+			'the-flash/2.jpg',
+			'the-flash/3.jpg',
+			'the-flash/4.jpg',
+		],
+		color: ['rgb(117, 10, 26)', 'rgb(222, 173, 54)', 'rgb(222, 173, 54)'],
 		bio:
 			'The Flash is one of the oldest on this list, with his origins dating back to 1940 (when he was simply called Flash). While he’s always been very popular in the comic books, it has taken decades for him to transfer to the big screen. There have been plenty of cartoons over the years and The Flash TV series on Netflix ran for 5 seasons which pleased his many fans.',
 		gender: 'male',
@@ -212,6 +289,13 @@ export let superhero = [
 		id: 17,
 		name: 'Invisible-Woman',
 		src: 'img/invisible-women.jpg',
+		pics: [
+			'invisible-women/1.jpg',
+			'invisible-women/2.jpg',
+			'invisible-women/3.jpg',
+			'invisible-women/4.jpg',
+		],
+		color: ['rgb(37, 52, 85)', 'rgb(54, 113, 179)', 'rgb(54, 113, 179)'],
 		bio:
 			'Marvel"s first superheroine may not have the highest profile of the characters on this list, but Sue Storm set the pace for modern female heroes – and still occupies a fairly unique place in comic books.',
 		gender: 'female',
@@ -225,6 +309,13 @@ export let superhero = [
 		id: 18,
 		name: 'Deadpool',
 		src: 'img/deadpool.jpg',
+		pics: [
+			'deadpool/1.jpg',
+			'deadpool/2.jpg',
+			'deadpool/3.jpg',
+			'deadpool/4.jpg',
+		],
+		color: ['rgb(1, 2, 10)', 'rgb(191, 23, 50)', 'rgb(191, 23, 50)'],
 		bio:
 			'Master martial artist, swordsman and marksman, Master strategist and improviser, has several voices in his head the represent parts of himself that give him advice, easily defies expectation in most instances, Accelerated Healing Factor, Extended longevity, Immunity to telepathy, teleportation (aided by machine), holographic disguise (aided by machine), Superhuman stamina, agility, flexibility and reflexes, may or may not be aware of the world beyond the fourth wall (IE, might know he is fictional)',
 		gender: 'male',
@@ -238,6 +329,8 @@ export let superhero = [
 		id: 19,
 		name: 'Batgirl',
 		src: 'img/batgirl.jpg',
+		pics: ['batgirl/1.jpg', 'batgirl/2.jpg', 'batgirl/3.jpg', 'batgirl/4.jpg'],
+		color: ['rgb(49, 56, 111)', 'rgb(255, 196, 88)', 'rgb(255, 196, 88)'],
 		bio:
 			'Barbara Gordon is unique among female heroes, and superheroes in general, for having not one but two vastly different and very successful superhero careers.Barbara started out as Batgirl, using her wits, her incredible intelligence, and her physical capabilities to earn Batman’s trust as an ally and protégé. However, after years of fighting crime on the streets of Gotham, a violent encounter with the Joker left her paralyzed – but not deterred.',
 		gender: 'female',
